459b33ee5bc8d0b118b8a21fd7eb3818950f0c52
Hassan Yahya Hussein <hassan@localhost.localdomain>
2018-06-28 14:53:42 +0300

    token handle

---
+++ app/handlers/token_handler.py
@@ -0,0 +1,37 @@
+"""Handle token validation"""
+from app.models.user_models import User
+from werkzeug.exceptions import HTTPException
+
+
+class NoTokenProvided(HTTPException):
+    """Raises:No token provided"""
+
+    def __init__(self):
+        HTTPException.__init__(self, "Please provide a token")
+        self.code = 499
+
+
+class InvalidToken(HTTPException):
+    "Raises:Invalid token provided"
+
+    def __init__(self, error):
+        HTTPException.__init__(self, str(error))
+        self.error = error
+        self.code = 498
+
+
+def assert_token(request):
+    """
+    Assert that a token was provided and is legit
+    :param request: Http request
+    :return: True else Exception
+    """
+
+    token = request.headers.get("Authorization")
+    if not token:
+        raise NoTokenProvided
+    user_id = User.decode_token(token)
+
+    if isinstance(user_id, str):
+        raise InvalidToken(user_id)
+    return user_id
