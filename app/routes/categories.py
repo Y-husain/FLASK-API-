418bcbe5eb2543e026300699426383d8e4339d8b
Hassan Yahya Hussein <hassan@localhost.localdomain>
2018-06-29 21:47:42 +0300

    [Update]category get routes

--- app/routes/categories.py
+++ app/routes/categories.py
@@ -7,8 +7,8 @@ from app import db
 category_blueprint = Blueprint('category', __name__)
 
 
-class CategoryAPI(MethodView):
-    """Categories Resource"""
+class CategoryAPI_POST(MethodView):
+    """This class handles get requests on category endpoint"""
 
     def post(self):
         """Handle post request to url /categories"""
@@ -41,8 +41,38 @@ class CategoryAPI(MethodView):
             return make_response(response), 400
 
 
+class CategoryAPI_GET(MethodView):
+    """This class handle post request on categories endpoints"""
+
+    def get(self):
+        """Handles get on endpoint /categories
+        :returns: 200 <ok>"""
+
+        user_id = assert_token(request)
+        categories_data = Category.query.filter_by(created_by=user_id)
+        result = []
+        for each_category in categories_data:
+            obj = {
+                "id": each_category.id,
+                "Recipe Category Name": each_category.name,
+                "Recipe Category Detail": each_category.detail,
+                "Date Created": each_category.date_created,
+                "Date Modified": each_category.date_modified
+            }
+            result.append(obj)
+        if not result:  # if result is empty
+            result.append("Nothing here yet")
+        response = jsonify(result)
+        status = 200 if result[0] != "Nothing here yet" else 222
+        return make_response(response), status
+
+
 base_url = '/v1/'
 # Post
-category_post_view = CategoryAPI.as_view('category_post_view')
+category_post_view = CategoryAPI_POST.as_view('category_post_view')
 category_blueprint.add_url_rule(
     base_url + 'categories', view_func=category_post_view, methods=['POST'])
+# Get
+category_get_view = CategoryAPI_GET.as_view('category_get_view')
+category_blueprint.add_url_rule(
+    base_url + 'categories', view_func=category_get_view, methods=['GET'])
